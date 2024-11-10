9303289d7476816274f0498f5a85dee416bb99d2
AhmedEssam0271 <ahmedessam0271@gmail.com>
2024-11-11 01:16:50 +0200

    3. Build a Simple Company Website with React

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+// import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar"; // (optional)
+// import Navbar from "./components/Navbar"; // (optional)
 
 function App() {
   return (
