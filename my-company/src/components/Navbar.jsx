1e324656ee037a93ea26b98bccd16dffeac5a9e5
AhmedEssam0271 <ahmedessam0271@gmail.com>
2024-11-11 01:44:30 +0200

    style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,18 +2,26 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav style={{ backgroundColor: "#333", padding: "10px" }}>
+      <ul
+        style={{
+          listStyle: "none",
+          margin: 0,
+          padding: "0px",
+          overflow: "hidden",
+          backgroundColor: "#333",
+        }}
+      >
+        <li style={{ float: "left", marginLeft: "5px" }}>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li style={{ float: "left", marginLeft: "5px" }}>
           <Link to="/about">About</Link>
         </li>
-        <li>
+        <li style={{ float: "left", marginLeft: "5px" }}>
           <Link to="/services">Services</Link>
         </li>
-        <li>
+        <li style={{ float: "left", marginLeft: "5px" }}>
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
