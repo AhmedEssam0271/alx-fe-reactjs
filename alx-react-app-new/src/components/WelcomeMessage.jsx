9d62b519abdf154520dabb88cc8bb8cddde4c93b
AhmedEssam0271 <ahmedessam0271@gmail.com>
2024-11-10 21:20:16 +0200

    alx-react-app-new

---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>&quot; Hello everyone, I am learning React at ALX! &quot;</h1>
+      <p>This is a simple JSX component.</p>
+      <p>&quot; I am learning about JSX! &quot;</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
