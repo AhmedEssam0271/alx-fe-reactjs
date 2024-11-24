e668938cfede862f3010bae463ad4960a2129b3a
AhmedEssam0271 <ahmedessam0271@gmail.com>
2024-11-24 17:45:06 +0200

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -12,7 +12,13 @@ const fetchPosts = async () => {
 const PostsComponent = () => {
   const { data, error, isLoading, isError, refetch } = useQuery(
     "posts",
-    fetchPosts
+    fetchPosts,
+    {
+      cacheTime: 1000 * 60 * 5, // 5 minutes
+      staleTime: 1000 * 60 * 1, // 1 minute
+      refetchOnWindowFocus: true,
+      keepPreviousData: true,
+    }
   );
 
   if (isLoading) {
